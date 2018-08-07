413f7572cb6ae1bf84dcc9efc633620ad8dd482d
Janion <Janion@192.168.2.3>
2018-08-07 19:17:31 +0100

    Added file log of python death

--- exploratory/pythonfile/Handler.java
+++ exploratory/pythonfile/Handler.java
@@ -114,6 +114,36 @@ public class Handler implements HttpHandler {
             while (process.isAlive());
             System.out.println("Python died");
         }).start();
+
+        new Thread(() -> {
+            BufferedWriter bw;
+            try {
+                bw = new BufferedWriter(new FileWriter(new File("/home/pi/table/PythonOutput.txt")));
+            } catch (IOException e) {
+                e.printStackTrace();
+                return;
+            }
+            while (true) {
+                BufferedReader br = null;
+                try {
+                    br = new BufferedReader(new InputStreamReader(process.getInputStream()));
+                    String line = br.readLine();
+                    bw.write(line);
+                } catch (IOException e) {
+                    e.printStackTrace();
+                    break;
+                } finally {
+                    try {
+                        if (br != null) {
+                            br.close();
+                        }
+                        bw.close();
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                }
+            }
+        }).start();
     }
 
     public void close() {
