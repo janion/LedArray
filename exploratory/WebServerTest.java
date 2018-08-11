787334c14ae83c5869dbc3bc1831ef3d24599ebf
Janion <Janion@192.168.2.3>
2018-08-11 11:38:36 +0100

    Updated simple web server test

--- exploratory/WebServerTest.java
+++ exploratory/WebServerTest.java
@@ -1,9 +1,27 @@
+import com.sun.net.httpserver.HttpExchange;
+import com.sun.net.httpserver.HttpHandler;
 import com.sun.net.httpserver.HttpServer;
 
 import java.io.IOException;
+import java.io.OutputStream;
 import java.net.InetSocketAddress;
 
 public class WebServerTest {
+
+    private class Handler implements HttpHandler {
+
+        @Override
+        public void handle(HttpExchange exchange) throws IOException {
+            System.out.println("Connection from: " + exchange.getRemoteAddress().getHostString());
+
+            String response = String.format("%s - %d", exchange.getRequestURI().getRawSchemeSpecificPart(), count++);
+
+            exchange.sendResponseHeaders(200, response.getBytes().length);
+            OutputStream os = exchange.getResponseBody();
+            os.write(response.getBytes());
+            os.close();
+        }
+    }
 
     private HttpServer server;
     private int count = 0;
@@ -11,7 +29,7 @@ public class WebServerTest {
     public WebServerTest() throws IOException {
         server = HttpServer.create(new InetSocketAddress(80), 0);
 
-        server.createContext("/", exchange -> String.format("%s - %d", exchange.getRequestURI().getRawSchemeSpecificPart(), count++));
+        server.createContext("/", new Handler());
 
         server.setExecutor(null); // creates a default executor
     }
